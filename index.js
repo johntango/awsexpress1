ac1190cba5b68a91192bc3466c36fd6d0970ef26
John Williams <johnwilliams@Johns-MacBook-Pro.local>
2019-02-01 08:14:11 -0500

    first

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-var a;
+var express = require('express');
+
+var app = express();
+app.get('/', function(req, res) {
+    var num = Math.floor(Math.random() * 10);
+    res.send('<h1>my lucky number is ' + num+'</h1>');
+});
+
+app.listen(3000);
+console.log("Running on port 3000");
